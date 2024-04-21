e7a0cc2bc5b57134c9eecb966ec541c552da2df6
bethropolis <bethropolis@gmail.com>
2024-04-21 15:54:34 +0300

    Update dependencies and add PWA support

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -7,39 +7,6 @@
 	onMount(overrideGlobalConsole);
 </script>
 
-<svelte:head>
-	<title>Mimo</title>
-	<link rel="icon" type="image/png" href="./web/favicon.ico" />
-
-	<!-- Primary Meta Tags -->
-	<title>Mimo — a simple programming language written in js.</title>
-	<meta name="title" content="Mimo — a simple programming language written in js." />
-	<meta
-		name="description"
-		content="Mimo is a programming language, with it's own built in lexer, parser and interpreter and can convert to multiple languages including javascript."
-	/>
-
-	<!-- Open Graph / Facebook -->
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://bethropolis.github.io/mimo/" />
-	<meta property="og:title" content="Mimo — a simple programming language written in js." />
-	<meta
-		property="og:description"
-		content="Mimo is a programming language, with it's own built in lexer, parser and interpreter and can convert to multiple languages including javascript."
-	/>
-	<meta property="og:image" content="https://bethropolis.github.io/mimo/web/mimo-banner.png" />
-
-	<!-- Twitter -->
-	<meta property="twitter:card" content="summary_large_image" />
-	<meta property="twitter:url" content="https://bethropolis.github.io/mimo/" />
-	<meta property="twitter:title" content="Mimo — a simple programming language written in js." />
-	<meta
-		property="twitter:description"
-		content="Mimo is a programming language, with it's own built in lexer, parser and interpreter and can convert to multiple languages including javascript."
-	/>
-	<meta property="twitter:image" content="https://bethropolis.github.io/mimo/web/mimo-banner.png" />
-	<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
-</svelte:head>
 
 <main>
 	<Sidebar />
--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,6 +1,50 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+			},
+			manifest: {
+				name: 'mimo playground',
+				short_name: 'mimo',
+				description: 'a simple programming language written in javascript.',
+				theme_color: '#1D232A',
+				icons: [
+					{
+						src: 'web/icon-192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'web/icon-192-maskable.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable'
+					},
+					{
+						src: 'web/icon-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: 'web/icon-512-maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			}
+		})
+	],
+
+	build: {
+		target: 'esnext'
+	}
 });
