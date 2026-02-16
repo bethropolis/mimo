9c966b0b3c0162896bffeeb2da8b452d7f0ca1d5
bethropolis <66518866+bethropolis@users.noreply.github.com>
2026-02-16 03:34:51 +0300

    update: preparing for mimo-next

--- compiler/execute/interpreters/while.js
+++
@@ -1,8 +0,0 @@
-import { interpretStatement } from "../interpreter.js";
-import { evaluate } from "../utils/evaluate.js";
-
-export async function interpretWhile(statement, env) {
-    while (evaluate(statement.condition, env)) {
-        if (await interpretStatement(statement.body, env)) return true;
-    }
-}
--- compiler/lexer/tokenTypes.js
+++
@@ -1,4 +0,0 @@
-// tokenTypes.js
-export const OPERATORS = ["+", "-", "*", "/", ">", "<", "=", "!"];
-export const KEYWORDS = ["let", "const", "if", "else", "for", "while", "function", "return"];
-export const PUNCTUATION = ["(", ")", ",", "[", "]"];
